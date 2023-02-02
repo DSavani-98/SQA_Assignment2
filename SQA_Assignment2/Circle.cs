cf489b17e1e2db7eb82d71c2dd2937ac44e982cf
Dhruv Savani <m_king_1604@Dhruvs-MacBook-Air.local>
2023-02-02 13:52:22 -0500

    completed Circle class

---
+++ SQA_Assignment2/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace SQA_Assignment2
+{
+    public class Circle
+    {
+        private int radius;
+        public Circle()
+        {
+            radius = 1;
+        }
+
+        public Circle(int newRadius)
+        {
+            radius = newRadius;
+        }
+
+        public int GetRadius()
+        {
+            return radius;
+        }
+
+        public void SetRadius(int newRadius)
+        {
+            radius = newRadius;
+        }
+
+        public double GetCircumference()
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
+
